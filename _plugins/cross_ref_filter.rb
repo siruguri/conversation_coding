09fa8961727237bc0a82afbff90f059b374fafbe
Sameer Siruguri <siruguri@gmail.com>
2014-09-06 23:40:00 -0700

    Added a border around images New post on HTTP - part 2 - with some images Cross Ref plugin now handles previous also

--- _plugins/cross_ref_filter.rb
+++ _plugins/cross_ref_filter.rb
@@ -4,7 +4,9 @@ module Jekyll
       # posts can point to next post
       current_post = @context.registers[:site].posts.select { |p| "#{url}" == "#{p.url}" }[0]
 
-      next_post_repl = next_post_cat_repl = '#'
+      prev_post_repl = next_post_repl = prev_post_cat_repl = next_post_cat_repl = '#'
+
+      # Walk forward, ... 
       if (next_post = current_post.next)
         next_post_repl = current_post.next.url
 
@@ -20,9 +22,28 @@ module Jekyll
         end
       end        
 
+      # And then backwards.
+      if (prev_post = current_post.previous)
+        next_post_repl = current_post.previous.url
+
+        found_cat_pred = false
+        while prev_post and !found_cat_pred do
+          diff = prev_post.categories - current_post.categories
+          if diff.count != prev_post.categories.count # something from the current post was in the prev post
+            found_cat_pred=true
+            prev_post_cat_repl = prev_post.url
+          else
+            prev_post = prev_post.previous
+          end
+        end
+      end        
+
       input = input.gsub(/\-\-next_post\-\-/, next_post_repl)
-      input.gsub(/\-\-next_post_by_cat\-\-/, next_post_cat_repl)
-      # 
+      input = input.gsub(/\-\-next_post_by_cat\-\-/, next_post_cat_repl)
+
+
+      input = input.gsub(/\-\-prev_post\-\-/, prev_post_repl)
+      input = input.gsub(/\-\-prev_post_by_cat\-\-/, prev_post_cat_repl)
     end
   end
 end
