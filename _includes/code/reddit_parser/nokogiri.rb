cf5868e8504c2948eda52d6c5d983c5295881b29
Sameer Siruguri <siruguri@gmail.com>
2014-10-15 18:44:17 -0700

    nokogiri parser

--- _includes/code/reddit_parser/nokogiri.rb
+++ _includes/code/reddit_parser/nokogiri.rb
@@ -6,6 +6,7 @@ require "sqlite3"
 
 # Open a database
 db_conn = SQLite3::Database.new "test.db"
+db_conn.results_as_hash=true
 
 stmt = SQLite3::Statement.new(db_conn, "CREATE  TABLE if not exists posts (title string, author_name text, date_posted datetime, category string, num_comments integer)")
 stmt.execute 
@@ -21,22 +22,35 @@ all_posts.each do |post|
   author_name = author_node.text
   post_time = timestamp.attr('datetime').to_s
 
-  category_node = post.css('a.subreddit')
-  cat_name = category_node.text
+  cat_name = post.css('span.domain a').text
 
   comments_node = post.css('a.comments')
   num_of_comments = comments_node.text.to_i
 
   puts "Title: #{title_node.text}, posted by #{author_name} at #{timestamp.attr('datetime')} into #{cat_name} with #{num_of_comments} comments"
 
-  stmt = SQLite3::Statement.\
-  new( \
-       db_conn,\
-       "INSERT INTO  posts (title, author_name, date_posted, category, num_comments) values (?, ?, ?, ?, ?)")
-  stmt.bind_params(title_node.text, author_name, post_time, cat_name, num_of_comments)
-  stmt.execute 
+
+  found_post=false
+
+  stmt = SQLite3::Statement.new(db_conn, "select * from posts where title=?")
+  stmt.bind_params title_node.text
+  rows=stmt.execute
+
+  found_post = (rows.count > 0)
   stmt.close
   
+  if(!found_post)
+    stmt = SQLite3::Statement.\
+    new( \
+         db_conn,\
+         "INSERT INTO  posts (title, author_name, date_posted, category, num_comments) values (?, ?, ?, ?, ?)")
+    stmt.bind_params(title_node.text, author_name, post_time, cat_name, num_of_comments)
+    stmt.execute 
+    stmt.close
+  else
+    puts "Already have a row with that title."
+  end
+
 end
 
 
